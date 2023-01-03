b026e0b19403a4c44584c81d12a5624eec51b3e7
amina21 <aminaabdelkafi93@gmail.com>
2023-01-03 19:38:16 +0000

    create navigation

--- my-react-app/apps/mobile/App.tsx
+++ my-react-app/apps/mobile/App.tsx
@@ -9,72 +9,22 @@ import WelcomeComponent from "./screen/WelcomeComponent";
 import HomeScreen from "./screen/HomeScreen";
 
 import { RoundButtonMusic } from "@my-workspace/my-ui";
-import { AntDesign , Ionicons , Entypo} from '@expo/vector-icons'; 
+import { AntDesign, Ionicons, Entypo } from '@expo/vector-icons';
 
 
 const App = () => {
 
-  const [isPlaying , setIsPlaying ] = useState(false)
-  const [duration , setDuration ] = useState(0)
-  const [count , setCount] = useState(0)
 
-  const ref = useRef("REF");
-
-  const handleClickButton = ()=>{
-    setIsPlaying(!isPlaying)
-    
-  }
-  let interval ;
-
-  useEffect (()=> {
-   console.log(" on monted")
-
-  },[])
-
-  useEffect(()=>{
-    console.log("useEffect ")
-    setCount(count => count+1)
-    console.log("arrayState",count)
-    if(isPlaying){
-      console.log(isPlaying)
-      interval = setInterval(()=>{
-        console.log("setInterval")
-        setDuration(oldValue => oldValue+1)
-      },1000)
-      
-    }
-    console.log("clearInterval")
-    return () => clearInterval(interval)
-  },[isPlaying ])
-
-  const handleRef = () => {
-    console.log("test", ref.current)
-  }
-
-  useEffect (()=> {
-    console.log(" array null -- ")
- 
-   })
 
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
-      {/* methode 1 */}
-<Text>Duration : {duration}</Text>
-<Text>REF : {ref.current}</Text>
-<Text>isPlaying : {isPlaying ? 'true' : 'false'}</Text>
-<Button onPress={()=>{ref.current = 'test'; handleRef() }} title="ref"/>
-
-{/* methode 2 */}
-{
-<RoundButtonMusic
- backgroundColor="blue"
- icon={isPlaying ? <AntDesign name="pause" size={24} color="white" /> : <AntDesign name="play" size={24} color="red" />}
- onClickButton={() => {handleClickButton()}}
-/>
-
-}
-
-</ApplicationProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Welcome" component={WelcomeComponent}></Stack.Screen>
+          <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 };
 
--- my-react-app/apps/mobile/screen/HomeScreen.tsx
+++ my-react-app/apps/mobile/screen/HomeScreen.tsx
@@ -1,19 +1,12 @@
-import { StyleSheet, Text, View,Button } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { CardFood } from "@my-workspace/my-ui";
 
-const HomeScreen = ({ navigation }) => {
-    return (
-        <View>
-            <CardFood
-                description={<Text style={{ color: 'black' }}>test</Text>}
-                price={<View style={{ flex: 1, flexDirection: 'row', height: 50 }}><Text style={{ color: 'black' }}>{' '}test{' '}</Text><Button title="+" /></View>}
-                title={<Text style={{ color: 'black' }}>test</Text>}
-            />
-
-            <Text>HomeScreen</Text>
-        </View>
-    )
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
 }
 
 export default HomeScreen
