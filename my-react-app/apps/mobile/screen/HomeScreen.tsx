8d779d96d65d510bb7a976a39cca3548bd756d5e
amina21 <aminaabdelkafi93@gmail.com>
2023-01-29 12:41:28 +0000

    add player

--- my-react-app/apps/mobile/screen/HomeScreen.tsx
+++ my-react-app/apps/mobile/screen/HomeScreen.tsx
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { UpNextButton, RoundButtonMusic , Cover} from '@my-workspace/my-ui'
+import { UpNextButton, RoundButtonMusic, Cover } from '@my-workspace/my-ui'
 import { AntDesign, Feather } from '@expo/vector-icons';
 import Slider from '@react-native-community/slider';
 import { FontAwesome } from '@expo/vector-icons';
 
-const HomeScreen = ({ navigation , route }) => {
+
+import { Video, AVPlaybackStatus } from 'expo-av';
+
+
+const HomeScreen = ({ navigation, route }) => {
     console.log(route)
+    const video = React.useRef(null);
+
     const [dataPlayer, setDataPlayer] = useState(route.params)
     const [currentDuration, setCurrentDuration] = useState(0)
     const [maxDuration, setMaxDuration] = useState(dataPlayer.duration)
@@ -26,7 +32,20 @@ const HomeScreen = ({ navigation , route }) => {
         timeString += `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
         return timeString;
     }
+    const handlePlay = (isPlaying) => {
+        setIsPlaying(isPlaying)
+        if (isPlaying) {
+            video.current.playAsync()
+        } else {
 
+           
+            video.current.pauseAsync()
+
+
+        }
+
+
+    }
 
 
     useEffect(() => {
@@ -54,8 +73,19 @@ const HomeScreen = ({ navigation , route }) => {
     }, [isPlaying])
 
     return (
-        <View style={{ flex: 1 , width : 350 , backgroundColor : "#1F1D2B" , justifyContent : 'center' , alignItems : 'center'}}>
+        <View style={{ flex: 1, width: 350, backgroundColor: "#1F1D2B", justifyContent: 'center', alignItems: 'center' }}>
             <Text>{name}</Text>
+
+            <Video
+                ref={video}
+                source={{
+                    uri: dataPlayer.urlMp4,
+                }}
+                useNativeControls
+                resizeMode="contain"
+                isLooping
+            />
+
             <View style={{ flex: 1, alignItems: 'center', flexDirection: 'row', justifyContent: 'center' }}>
 
                 <RoundButtonMusic
@@ -69,16 +99,16 @@ const HomeScreen = ({ navigation , route }) => {
             <View style={{ flex: 4 }}>
 
 
-            <Cover
-  borderRadius={10}
-  imageUrl={dataPlayer.poster}
-/>                
+                <Cover
+                    borderRadius={10}
+                    imageUrl={dataPlayer.poster}
+                />
 
-                
+
             </View>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
-                <View style={{ flex: 1, minWidth: 300, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+                <View style={{ flex: 1, minWidth: 300, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
                     <Text style={{ color: 'white' }}>{convertSecondToHHMMSS(currentDuration)}</Text>
                     <Text style={{ color: 'white' }}>{convertSecondToHHMMSS(maxDuration - currentDuration)}</Text>
 
@@ -96,7 +126,7 @@ const HomeScreen = ({ navigation , route }) => {
                     thumbTintColor="#F00DFD"
                 />
             </View>
-            <View style={{ flex: 1, flexDirection: "row", alignItems: 'center', justifyContent: 'center'}}>
+            <View style={{ flex: 1, flexDirection: "row", alignItems: 'center', justifyContent: 'center' }}>
                 <RoundButtonMusic
                     backgroundColor="blue"
                     icon={<Feather name="chevrons-left" size={24} color="white" />}
@@ -112,7 +142,7 @@ const HomeScreen = ({ navigation , route }) => {
                 /> <RoundButtonMusic
                     backgroundColor="blue"
                     icon={isPlaying ? <FontAwesome name="pause" size={24} color="white" /> : <FontAwesome name="play" size={24} color="white" />}
-                    onClickButton={() => { setIsPlaying(!isPlaying) }}
+                    onClickButton={() => { handlePlay(!isPlaying) }}
                     size={50}
                 /> <RoundButtonMusic
                     backgroundColor="blue"
--- my-react-app/apps/mobile/screen/PlaylistScreen.tsx
+++ my-react-app/apps/mobile/screen/PlaylistScreen.tsx
@@ -4,14 +4,14 @@ import React, { useState } from 'react'
 const PlaylistScreen = ({ navigation }) => {
     const [playlist] = useState([{
         name: "video 1",
-        urlMp4 : "https://----.mp4",
+        urlMp4 : "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
         duration : 70, 
         poster : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2020%2F01%2Feminem-1-2000.jpg"
 
 
     }, {
         name: "video 2",
-        urlMp4 : "https://----.mp4",
+        urlMp4 : "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
         duration : 70,
         poster : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2020%2F01%2Feminem-1-2000.jpg"
 
